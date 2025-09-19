b9459b190290b36623f07d59aafb89637ef5e6db
simalbutt <138391970+simalbutt@users.noreply.github.com>
2025-09-20 02:09:29 +0500

    add employee table UI

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 
 // connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.error('❌ MongoDB error:', err));
+  .then(() => console.log(' MongoDB connected'))
+  .catch(err => console.error(' MongoDB error:', err));
 
 // use routes
 app.use('/api', employeeRoutes);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
